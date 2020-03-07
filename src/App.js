b7eaf7c8e1f1d55447e7702b8db34093ef06a7d6
mb0606 <marcoberardini@gmail.com>
2020-03-07 15:47:44 -0800

    Creating SearchInput component and adding to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Components/Header";
+import SearchInput from "./Components/SearchInput";
 
 function App() {
   return (
     <div>
       <Header />
+      <SearchInput />
     </div>
   );
 }
