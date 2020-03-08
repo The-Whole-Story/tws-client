9a6a2f7e20ec3feeb54ec271dfdf816ced2fc0e9
mb0606 <marcoberardini@gmail.com>
2020-03-07 16:00:08 -0800

    Adding input field and testing state on change

--- src/Components/SearchInput.js
+++ src/Components/SearchInput.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SearchInput() {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
   return (
     <div>
       <h1>SearchInput JS</h1>
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <h2>{searchTerm}</h2>
     </div>
   );
 }
