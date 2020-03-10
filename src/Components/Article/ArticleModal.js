92f3810424bf854a7c8795f1fc80ee9b9d0f0e5c
mb0606 <marcoberardini@gmail.com>
2020-03-09 17:24:25 -0700

    Adding progress bar to modal

--- src/Components/Article/ArticleModal.js
+++ src/Components/Article/ArticleModal.js
@@ -1,11 +1,11 @@
 import React from "react";
 import mime from "mime-types";
-import { Modal, Image, Button, Icon, Header } from "semantic-ui-react";
+import { Modal, Image, Progress, Icon, Header } from "semantic-ui-react";
 
 function ArticleModal({ modal, closeModal, vote }) {
   return (
     <Modal open={modal} onClose={closeModal}>
-      <Modal.Header>You {vote} from the BBC. Are you suprised ?</Modal.Header>
+      <Modal.Header>The source of this article is BBC</Modal.Header>
       <Modal.Content image>
         <Image
           wrapped
@@ -13,8 +13,20 @@ function ArticleModal({ modal, closeModal, vote }) {
           src="https://react.semantic-ui.com/images/avatar/large/rachel.png"
         />
         <Modal.Description>
-          <Header>Move to the next article to get the whole story.</Header>
-          <Icon size="huge" name="arrow alternate circle right"></Icon>
+          <Header>
+            Are you suprised {vote} voted this article? Move to the next article
+            to get the whole story.
+          </Header>
+          <Progress value="3" total="5" progress="ratio" active>
+            You're almost there
+          </Progress>
+          <div style={{ textAlign: "right", paddingTop: "10px" }}>
+            <Icon
+              size="huge"
+              name="arrow alternate circle right"
+              color="grey"
+            ></Icon>
+          </div>
         </Modal.Description>
       </Modal.Content>
     </Modal>
