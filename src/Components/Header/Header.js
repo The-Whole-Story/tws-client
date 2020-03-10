b05d1a9b1301655d02626c2c9e32f6abb6d76500
mb0606 <marcoberardini@gmail.com>
2020-03-09 22:09:37 -0700

    Replacing header menu with semantic ui Menu component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,37 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../LOGO-whole-story.png";
-import { Divider, Image } from "semantic-ui-react";
+import { Image, Menu } from "semantic-ui-react";
 
 function Header() {
+  const [activeItem, setActiveItem] = useState("");
+
+  const handleItemClick = active => {
+    setActiveItem(active);
+  };
   return (
     <React.Fragment>
       <Link to={`/`} className="text">
         <Image src={Logo} centered />{" "}
       </Link>
-
-      <div
-        className="ui borderless menu"
-        style={{
-          marginBottom: "20px",
-          border: "none",
-          boxShadow: "none"
-        }}
-      >
-        <div className="ui centered grid container">
-          <div className="ui item">
-            <Link to={`/about`} className="text">
-              ABOUT US
-            </Link>
-          </div>
-
-          <div className="ui  item">
-            <Link to={`/basics`} className="text">
-              101
-            </Link>
-          </div>
-        </div>
-      </div>
+      <Menu pointing secondary fluid widths={6}>
+        <Menu.Item
+          as={Link}
+          to="/about"
+          name="About"
+          active={activeItem === "about"}
+          onClick={() => handleItemClick("about")}
+        />
+        <Menu.Item
+          as={Link}
+          to="/basics"
+          name="101"
+          active={activeItem === "basics"}
+          onClick={() => handleItemClick("basics")}
+        />
+        <Menu.Item
+          name="Contact Us"
+          active={activeItem === "Contact"}
+          onClick={() => handleItemClick("Contact")}
+        />
+      </Menu>
     </React.Fragment>
   );
 }
