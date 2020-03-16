69d3aeb37ce28e2c9ac3134111b5f3acbcfd2c5d
mb0606 <marcoberardini@gmail.com>
2020-03-16 11:10:00 -0700

    Refactoring API calls

--- src/API/twsApi.js
+++ src/API/twsApi.js
@@ -1,43 +1,41 @@
-const axios = require('axios');
+const axios = require("axios");
 
-export const fetchSubtopics = async (topic) => {
-    try {
-        const result = await axios({
-            method: 'post',
-            url: 'https://tws-node-server.herokuapp.com/subtopics',
-            data: { nSubtopics: 18, topic: topic },
-            headers: { twsKey: process.env.REACT_APP_TWS_KEY }
-        });
-        return result.data; //result.data is an array of strings, the subtopics
-    } catch (err) {
-        console.log(err);
-    }
+export const fetchSubtopics = async (topic, nSubtopics) => {
+  console.log("API key ", process.env.REACT_APP_TWS_KEY);
+  try {
+    const result = await axios({
+      method: "get",
+      url: `https://tws-node-server.herokuapp.com/subtopics?nResults=${nSubtopics}&topic=${topic}`,
+      headers: { twsKey: process.env.REACT_APP_TWS_KEY }
+    });
+    return result.data; //result.data is an array of strings, the subtopics
+  } catch (err) {
+    console.error(err);
+  }
 };
 
-export const fetchArticleIds = async (query) => {
-    try {
-        const result = await axios({
-            method: 'post',
-            url: 'https://tws-node-server.herokuapp.com/articleIds',
-            data: { nArticles: 10, query: query },
-            headers: { twsKey: process.env.REACT_APP_TWS_KEY }
-        });
-        return result.data; //result.data is an array of ints, the articleId s
-    } catch (err) {
-        console.log(err);
-    }
+export const fetchArticleIds = async (query, nArticles) => {
+  try {
+    const result = await axios({
+      method: "get",
+      url: `https://tws-node-server.herokuapp.com/articleIds?nResults=${nArticles}&query=${query}`,
+      headers: { twsKey: process.env.REACT_APP_TWS_KEY }
+    });
+    return result.data; //result.data is an array of ints, the articleId s
+  } catch (err) {
+    console.error(err);
+  }
 };
 
-export const fetchArticlesById = async (ids) => {
-    try {
-        const result = await axios({
-            method: 'post',
-            url: 'https://tws-node-server.herokuapp.com/articlesById',
-            data: { ids: ids },
-            headers: { twsKey: process.env.REACT_APP_TWS_KEY }
-        });
-        return result.data; //result.data is an array of objects, each object is an article
-    } catch (err) {
-        console.log(err);
-    }
+export const fetchArticlesById = async id => {
+  try {
+    const result = await axios({
+      method: "post",
+      url: `https://tws-node-server.herokuapp.com/articlesById?id1=${id}`,
+      headers: { twsKey: process.env.REACT_APP_TWS_KEY }
+    });
+    return result.data; //result.data is an array of objects, each object is an article
+  } catch (err) {
+    console.error(err);
+  }
 };
