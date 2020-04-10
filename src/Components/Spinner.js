7e7e9225fab496e7d851b2a8b167facfa763b084
Talia <taliabahar@yahoo.com>
2020-04-10 15:12:34 -0700

    fixed spinner issues

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -2,8 +2,12 @@ import React from "react";
 import { Loader, Dimmer } from "semantic-ui-react";
 
 const Spinner = ({ message }) => (
-  <Dimmer active>
-    <Loader size="huge" content={message ? message : "Loading... "} />
+  <Dimmer active inverted>
+    <Loader
+      inline="centered"
+      size="huge"
+      content={message ? message : "Loading... "}
+    />
   </Dimmer>
 );
 export default Spinner;
