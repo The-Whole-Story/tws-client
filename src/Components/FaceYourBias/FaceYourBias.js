b005a17f5e67941bcce6bf5dce8f4de4179b29d0
anacanm <anacan@mangelsdorf.io>
2020-04-25 20:26:34 -0700

    Sort bias leanings by magnitude

--- src/Components/FaceYourBias/FaceYourBias.js
+++ src/Components/FaceYourBias/FaceYourBias.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function FaceYourBias() {
-    return <h1>Howdy!</h1>;
+    const leaningScores = {
+        left: parseInt(localStorage.getItem('left')),
+        neutral: parseInt(localStorage.getItem('neutral')),
+        right: parseInt(localStorage.getItem('right')),
+    };
+    const sortedMagnitudes = Object.keys(leaningScores).sort((a, b) => {
+        // sorts in decreasing magnitude: first element will be highest
+        if (Math.abs(leaningScores[a]) > Math.abs(leaningScores[b])) {
+            return -1;
+        } else if (Math.abs(leaningScores[a]) < Math.abs(leaningScores[b])) {
+            return 1;
+        }
+        return 0;
+    });
 }
 
 export default FaceYourBias;
