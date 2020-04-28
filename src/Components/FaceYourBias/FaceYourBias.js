101efe976ebe85a32b62b5853810df1a7c34aea3
anacanm <anacan@mangelsdorf.io>
2020-04-27 19:26:22 -0700

    Add simple liked/disliked message for leaning scores

--- src/Components/FaceYourBias/FaceYourBias.js
+++ src/Components/FaceYourBias/FaceYourBias.js
@@ -15,6 +15,23 @@ function FaceYourBias() {
         }
         return 0;
     });
+
+    const likedDisliked = (num, direction) => {
+        if (num < 0) {
+            return `You disliked ${Math.abs(num)} more ${direction}-leaning articles than you liked.`;
+        } else if (num > 0) {
+            return `You liked ${Math.abs(num)} more ${direction}-leaning articles than you liked.`;
+        } else {
+            return `You were perfectly balanced for ${direction}-leaning articles!.`;
+        }
+    };
+    return (
+        <div>
+            <h1>{likedDisliked(parseInt(localStorage.getItem('left')), 'left')}</h1>{' '}
+            <h1>{likedDisliked(parseInt(localStorage.getItem('neutral')), 'neutral')}</h1>{' '}
+            <h1>{likedDisliked(parseInt(localStorage.getItem('right')), 'right')}</h1>{' '}
+        </div>
+    );
 }
 
 export default FaceYourBias;
